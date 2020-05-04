a28ae6ec3eea96a205bdf0a6d0016c526135dc94
Saroshmalik <47161449+Saroshmalik@users.noreply.github.com>
2020-05-04 01:33:08 -0400

    Sales_Calcutor

---
+++ Sales_Calculator/Sales_Calculator/Form1.Designer.vb
@@ -0,0 +1,190 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btn_LoadFile = New System.Windows.Forms.Button()
+        Me.Lst_Track = New System.Windows.Forms.ListBox()
+        Me.txt_Purchase = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Btn_Purchase = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txt_Sub = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.txt_Tax = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Txt_Sale_Total = New System.Windows.Forms.TextBox()
+        Me.btn_Reset = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btn_LoadFile
+        '
+        Me.btn_LoadFile.Location = New System.Drawing.Point(16, 12)
+        Me.btn_LoadFile.Name = "btn_LoadFile"
+        Me.btn_LoadFile.Size = New System.Drawing.Size(122, 35)
+        Me.btn_LoadFile.TabIndex = 0
+        Me.btn_LoadFile.Text = "&Load File"
+        Me.btn_LoadFile.UseVisualStyleBackColor = True
+        '
+        'Lst_Track
+        '
+        Me.Lst_Track.FormattingEnabled = True
+        Me.Lst_Track.ItemHeight = 20
+        Me.Lst_Track.Location = New System.Drawing.Point(12, 112)
+        Me.Lst_Track.Name = "Lst_Track"
+        Me.Lst_Track.Size = New System.Drawing.Size(180, 224)
+        Me.Lst_Track.TabIndex = 1
+        '
+        'txt_Purchase
+        '
+        Me.txt_Purchase.Location = New System.Drawing.Point(403, 77)
+        Me.txt_Purchase.Name = "txt_Purchase"
+        Me.txt_Purchase.Size = New System.Drawing.Size(165, 26)
+        Me.txt_Purchase.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(222, 77)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(164, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Enter Purchase Value"
+        '
+        'Btn_Purchase
+        '
+        Me.Btn_Purchase.Location = New System.Drawing.Point(484, 112)
+        Me.Btn_Purchase.Name = "Btn_Purchase"
+        Me.Btn_Purchase.Size = New System.Drawing.Size(84, 33)
+        Me.Btn_Purchase.TabIndex = 4
+        Me.Btn_Purchase.Text = "&Purchase"
+        Me.Btn_Purchase.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 77)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(100, 20)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Puchase List"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(222, 163)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(77, 20)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Sub Total"
+        '
+        'txt_Sub
+        '
+        Me.txt_Sub.Enabled = False
+        Me.txt_Sub.Location = New System.Drawing.Point(403, 163)
+        Me.txt_Sub.Name = "txt_Sub"
+        Me.txt_Sub.Size = New System.Drawing.Size(165, 26)
+        Me.txt_Sub.TabIndex = 6
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(222, 212)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(94, 20)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Tax Amount"
+        '
+        'txt_Tax
+        '
+        Me.txt_Tax.Enabled = False
+        Me.txt_Tax.Location = New System.Drawing.Point(403, 212)
+        Me.txt_Tax.Name = "txt_Tax"
+        Me.txt_Tax.Size = New System.Drawing.Size(165, 26)
+        Me.txt_Tax.TabIndex = 8
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(222, 261)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(88, 20)
+        Me.Label5.TabIndex = 11
+        Me.Label5.Text = "Sales Total"
+        '
+        'Txt_Sale_Total
+        '
+        Me.Txt_Sale_Total.Enabled = False
+        Me.Txt_Sale_Total.Location = New System.Drawing.Point(403, 261)
+        Me.Txt_Sale_Total.Name = "Txt_Sale_Total"
+        Me.Txt_Sale_Total.Size = New System.Drawing.Size(165, 26)
+        Me.Txt_Sale_Total.TabIndex = 10
+        '
+        'btn_Reset
+        '
+        Me.btn_Reset.Location = New System.Drawing.Point(484, 12)
+        Me.btn_Reset.Name = "btn_Reset"
+        Me.btn_Reset.Size = New System.Drawing.Size(94, 35)
+        Me.btn_Reset.TabIndex = 12
+        Me.btn_Reset.Text = "&Reset"
+        Me.btn_Reset.UseVisualStyleBackColor = True
+        '
+        'form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(590, 395)
+        Me.Controls.Add(Me.btn_Reset)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Txt_Sale_Total)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txt_Tax)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.txt_Sub)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Btn_Purchase)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_Purchase)
+        Me.Controls.Add(Me.Lst_Track)
+        Me.Controls.Add(Me.btn_LoadFile)
+        Me.Name = "form1"
+        Me.Text = "Sales Calculator"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents btn_LoadFile As Button
+    Friend WithEvents Lst_Track As ListBox
+    Friend WithEvents txt_Purchase As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Btn_Purchase As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txt_Sub As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txt_Tax As TextBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Txt_Sale_Total As TextBox
+    Friend WithEvents btn_Reset As Button
+End Class
---
+++ Sales_Calculator/Sales_Calculator/Form1.vb
@@ -0,0 +1,157 @@
+﻿''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+' This program Is used To calculate the sales, First user need To                '
+' load sales prices into the application from saleList.txt file then             '
+' user is allow to enter the sale price from saleList.txt which should           '
+' be multiple of 10 (start from 10 to 100) and tax price will be calculated as   '
+' if sub total is less than 50 then tax percentage is 10 and if sub total is     '
+' greater than 50 and less than equal to 100 then tax percentage is 15 and if the'
+' sub total Is greater than 100 and less than equal to 200 then txt percentage   '
+' will be 20 And If Sub total Is greater Then 200 then tax percentage will 25    '
+' percentage of sub total.                                                       '
+''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+Imports System.IO
+
+Public Class form1
+    ' This array variable is used to load the Price list from salesList.txt file
+    Dim Price_List(9) As Integer
+
+    ' This variable is used to store the subtotal of purchase.
+    Dim subTotal As Integer = 0
+
+    ' On Button click load the item prices from saleList.txt.
+    Private Sub btn_LoadFile_Click(sender As Object,
+                        e As EventArgs) Handles btn_LoadFile.Click
+
+        ' Getting the path of current directory. 
+        Dim path As String = Directory.GetCurrentDirectory()
+        ' Set the path for saleList.txt. 
+        Dim strFileName() As String = File.ReadAllLines(path + "\salesList.txt")
+
+        ' Set the index value to 0.
+        Dim index As Integer = 0
+
+        ' Read each line from saleList.txt file.
+        For Each currentLine In strFileName
+            ' store the prices from salesList.txt file to Price_List array.
+            Price_List(index) = Convert.ToInt32(currentLine)
+
+            ' Increase the index value by 1 to get the 
+            ' Next value from saleList.txt file.
+            index = index + 1
+        Next
+
+        ' Display message to user.
+        MessageBox.Show("Price List is loaded")
+    End Sub
+
+    ' In sub we are calculating values.
+    Private Sub Btn_Purchase_Click(sender As Object,
+                    e As EventArgs) Handles Btn_Purchase.Click
+
+        ' check if puchase text box is empty
+        If (txt_Purchase.Text = "") Then ' Then
+            ' Display the error message to user.
+            MessageBox.Show("Please enter puchase value")
+        End If
+
+        ' This variable is used to store the purchased value enter by user. 
+        Dim value As Integer = Convert.ToInt32(txt_Purchase.Text)
+
+        ' This variable is used to check if user enters the correct 
+        ' purchase value.
+        Dim found As Boolean = False
+
+        ' Check the puchase value into Price_List
+        For Each element In Price_List
+            ' Check if user enters value is equal to current value in
+            ' Price List.
+            If (Convert.ToInt32(element) = value) Then
+                found = True ' If yes then set the found to true
+                Exit For
+            End If
+        Next
+
+        ' Check if the value exists in Price List.
+        If (found) Then ' If yes then.
+            ' store the value into list box.
+            Lst_Track.Items.Add(value)
+
+            ' Calculate the sub total.
+            subTotal = subTotal + value
+
+            ' Set the subTotal text value.
+            txt_Sub.Text = subTotal.ToString()
+
+            ' Get the tax Percentage.
+            Dim taxPercentage As Integer = calculateTax()
+
+            ' Calculate the tax price.
+            Dim taxPrice As Double = Convert.ToDouble(
+                (taxPercentage * subTotal) / 100)
+
+            ' Set the txt_Tax text value.
+            txt_Tax.Text = taxPrice.ToString()
+
+            ' Get the Sale Total Price.
+            Dim saleTotal As Double = calculateSaleTotal(taxPrice)
+
+            ' Set the Sale Total price to text box.
+            Txt_Sale_Total.Text = saleTotal.ToString()
+        Else
+            ' Display the error message to user.
+            MessageBox.Show("Invalid purchase value Enter " +
+                    "multiple of 10 (start from 10 to 100)")
+        End If
+    End Sub
+
+    ' This Function is used to calculate and return the sale total value.
+    Public Function calculateSaleTotal(
+                               ByVal taxPrice As Double) As Double
+
+        ' Calculate the sale total value.
+        Dim saleTotal As Double = taxPrice + subTotal
+
+        ' return the sale total.
+        Return saleTotal
+    End Function
+
+    ' This method is used to calculate the tax.
+    Public Function calculateTax() As Integer
+
+        ' This variable is used to store the tax percentage.
+        Dim taxPercentage As Integer = 0
+
+        ' Check if sub total is greater than 0 and less than equal to 50.
+        If (subTotal <= 50 And subTotal > 0) Then ' If yes then
+            taxPercentage = 10 ' Set the tax percentage to 10.
+
+            ' Check if sub total is greater than 50 and less than equal to 100.
+        ElseIf (subTotal > 50 And subTotal <= 100) Then
+            taxPercentage = 15 ' Set the tax percentage to 15.
+            ' Check if sub total is greater than 100 and less than equal to 200.
+        ElseIf (subTotal > 100 And subTotal <= 200) Then
+            taxPercentage = 20 ' Set the tax Percentage to 20.
+        Else ' If subtotal is greated than 200 than.
+            taxPercentage = 25 ' Set the tax percentage to 25
+        End If
+        Return taxPercentage ' return the tax percentage.
+        Return taxPercentage ' return the tax percentage.
+        Return taxPercentage ' return the tax percentage.
+        Return taxPercentage ' return the tax percentage.
+        Return taxPercentage ' return the tax percentage.
+    End Function
+
+    ' This method is used to reset the values.
+    Private Sub btn_Reset_Click(sender As Object,
+                    e As EventArgs) Handles btn_Reset.Click
+
+        ' Clear the List box value.
+        Lst_Track.Items.Clear()
+        subTotal = 0 ' Set sub total to 0.
+        txt_Purchase.Text = "" ' clear the purchase text box.
+        Txt_Sale_Total.Text = "" ' Clear the sale total text box.
+        txt_Sub.Text = "" ' Clear the sub total text box.
+        txt_Tax.Text = "" ' Clear the tax text box value.
+    End Sub
+End Class
